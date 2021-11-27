b3e8c942f0a39839cdf9ee09d715d509bc0741be
Siegfred <dante_sparda1925@yahoo.com>
2021-11-28 01:39:03 +0200

    created a master layout component

--- src/master/Master.js
+++ src/master/Master.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import SampleImage from '../images/portfoliopic.png'
 import './Master.css'
 
 const Master = () => {
     return (
-        <div>
-            <h1>Master</h1>
+        <div className="master-container">
+            <div className="card">
+                <div className="sample-img">
+                    <img src={SampleImage} alt=""/>
+                </div>
+                <div className="sample-info">
+                    <h2>Name: Siegfred Gamboa</h2>
+                    <h3>Bio</h3>
+                    <p> Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</p>
+                </div>
+            </div>
+            <div className="card">
+                <div className="sample-img">
+                    <img src={SampleImage} alt=""/>
+                </div>
+                <div className="sample-info">
+                    <h2>Name: Siegfred Gamboa</h2>
+                    <h3>Bio</h3>
+                    <p> Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</p>
+                </div>
+            </div>
+            <div className="card">
+                <div className="sample-img">
+                    <img src={SampleImage} alt=""/>
+                </div>
+                <div className="sample-info">
+                    <h2>Name: Siegfred Gamboa</h2>
+                    <h3>Bio</h3>
+                    <p> Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</p>
+                </div>
+            </div>
+            <div className="card">
+                <div className="sample-img">
+                    <img src={SampleImage} alt=""/>
+                </div>
+                <div className="sample-info">
+                    <h2>Name: Siegfred Gamboa</h2>
+                    <h3>Bio</h3>
+                    <p> Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</p>
+                </div>
+            </div>
+            <div className="card">
+                <div className="sample-img">
+                    <img src={SampleImage} alt=""/>
+                </div>
+                <div className="sample-info">
+                    <h2>Name: Siegfred Gamboa</h2>
+                    <h3>Bio</h3>
+                    <p> Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</p>
+                </div>
+            </div>
         </div>
     )
 }
